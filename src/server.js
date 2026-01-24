c3e1c4909079b95b5aa5f494e57f116f7bd361d9
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2026-01-24 12:43:15 +0530

    Day 2: MongoDB connected and User model created

--- src/server.js
+++ src/server.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const {ApolloServer,gql} = require("apollo-server-express");
-require('dotenv').config();
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const User = require("./models/User");
+require("dotenv").config();
 
 // attempt to connect to the database
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 connectDB();
 
+
 const typeDefs = gql`
-type Query{
-  hello: String
-}
+  type Query {
+    hello: String
+  }
 `;
 
-const resolvers ={
-    Query:{
-        hello:() => 'Hello world!'
-    }
-}
-
-async function startServer(){
-    const app = express();
-
-    const server = new ApolloServer({typeDefs,resolvers});
-    await server.start();
-    server.applyMiddleware({app, path:'/graphql'});
-
-    app.listen(4000,()=>{
-        console.log('Server is running on http://localhost:4000/graphql');
-    })
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
 };
+
+async function startServer() {
+  const app = express();
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(4000, () => {
+    console.log("Server is running on http://localhost:4000/graphql");
+  });
+}
 
 startServer();
