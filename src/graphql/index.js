3f4116402df452deaaaf7e507b87bfb5e1f96fa9
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2026-02-08 19:09:58 +0530

    Day 8: Add analytics type definitions and resolvers to GraphQL schema

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -6,10 +6,12 @@ const transactionTypeDefs = require('./typeDefs/transaction.typeDefs');
 const transactionResolvers = require('./resolvers/transaction.resolvers');
 const payeeTypeDefs = require('./typeDefs/payee.typeDefs');
 const payeeResolvers = require('./resolvers/payee.resolvers');
+const analyticsResolvers = require('./resolvers/analytics.resolvers');
+const analyticsTypeDefs = require('./typeDefs/analytics.typeDefs');
 
 // Include payee typeDefs and resolvers
-const typeDefsArray = [userTypeDefs, transactionTypeDefs, payeeTypeDefs];
-const resolversArray = [userResolvers, transactionResolvers, payeeResolvers];   
+const typeDefsArray = [userTypeDefs, transactionTypeDefs, payeeTypeDefs,analyticsTypeDefs];
+const resolversArray = [userResolvers, transactionResolvers, payeeResolvers, analyticsResolvers];   
 // Merging ensures nested objects (like Query/Mutation) are unified
 const typeDefs = mergeTypeDefs(typeDefsArray);
 const resolvers = mergeResolvers(resolversArray);
