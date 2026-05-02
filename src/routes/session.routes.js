7dbebcf2df6ec690b36fc16b603e771385515f8d
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2026-05-02 22:53:45 +0530

    refactor: update session route to RESTful DELETE /api/session

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { clearSession } = require("../controllers/sessionController");
 
-router.delete("/clear", clearSession);
+router.delete("/", clearSession);
 
 module.exports = router;
