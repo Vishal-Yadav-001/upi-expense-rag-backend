cac6cb7d7f47e2261b4a0bffc4af469072d69c6f
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2026-05-04 19:49:26 +0530

    fix: correct session clear route path to match tests

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { clearSession } = require("../controllers/sessionController");
 
-router.delete("/", clearSession);
+router.delete("/clear", clearSession);
 
 module.exports = router;
